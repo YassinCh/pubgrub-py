828b252931ca0760b0df127595bd24ea6464595d
Yassin Chibrani <yassin@MacBook-Pro.local>
2026-02-05 17:40:49 +0100

    chore: bump version to 1.1.0

--- python/pubgrub_py/__init__.py
+++ python/pubgrub_py/__init__.py
@@ -4,4 +4,4 @@ from pubgrub_py._core import ResolutionError, Resolver
 from pubgrub_py.resolve import resolve
 
 __all__ = ["ResolutionError", "Resolver", "resolve"]
-__version__ = "1.0.0"
+__version__ = "1.1.0"
